9c34f3b3d421c533367bae83969d7b4f5f17caf6
Marit Zelinsky <maritzelinsky@me.com>
2019-07-23 15:47:55 -0500

    completed rating bonus and seniority bonus

--- client.js
+++ client.js
@@ -33,6 +33,45 @@ const employees = [
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
+let ratingBonus = [];
+let addBonus = [];
+
+function bonus(employeesArray){
+  for(let i = 0; i < employeesArray.length; i++){
+    if (employeesArray[i].reviewRating == 5){
+      console.log('This is a review bonus', employeesArray[i].annualSalary * .1);
+    }
+    else if (employeesArray[i].reviewRating == 3){
+      console.log('This is a review bonus',employeesArray[i].annualSalary * .04);
+    }
+    else if (employeesArray[i].reviewRating == 4){
+      console.log('This is a review bonus',employeesArray[i].annualSalary * .06);
+    }
+    else {
+      console.log('No bonus.');
+    }
+  }
+}
+ bonus(employees);
+
+//check for employee Number length with loop 
+// if employee number = 4 digits then recieve addtional 5%
+// adjust bonus if salary over $65k -1%
+
+function seniority(additionalBonus) {
+  for (let i = 0; i < additionalBonus.length; i++) {
+    if(additionalBonus[i].employeeNumber.length == 4) {
+      console.log('additionalBonus', additionalBonus[i].annualSalary * .05); 
+    }
+    else {
+      console.log('no additional bonus');
+    }
+  
+}
+}
+
+seniority(employees);
+
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
 // What is the fewest lines of code I can write and test to get just a little closer?
@@ -40,4 +79,5 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+
+
