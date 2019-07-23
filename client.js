6794c375ebd1da16f65c8749381da7bc2e4c3019
Marit Zelinsky <maritzelinsky@me.com>
2019-07-23 16:37:50 -0500

    working done in class

--- client.js
+++ client.js
@@ -36,23 +36,24 @@ const employees = [
 let ratingBonus = [];
 let addBonus = [];
 
-function bonus(employeesArray){
-  for(let i = 0; i < employeesArray.length; i++){
-    if (employeesArray[i].reviewRating == 5){
-      console.log('This is a review bonus', employeesArray[i].annualSalary * .1);
+function bonus(employeesArray) {
+  for (let i = 0; i < employeesArray.length; i++) {
+    if (employeesArray[i].reviewRating == 5) {
+      ratingBonus.push(employeesArray[i].annualSalary * .1);
     }
-    else if (employeesArray[i].reviewRating == 3){
-      console.log('This is a review bonus',employeesArray[i].annualSalary * .04);
+    else if (employeesArray[i].reviewRating == 3) {
+      ratingBonus.push(employeesArray[i].annualSalary * .04);
     }
-    else if (employeesArray[i].reviewRating == 4){
-      console.log('This is a review bonus',employeesArray[i].annualSalary * .06);
+    else if (employeesArray[i].reviewRating == 4) {
+      ratingBonus.push(employeesArray[i].annualSalary * .06);
     }
     else {
-      console.log('No bonus.');
+      ratingBonus.push('No bonus');
     }
   }
 }
- bonus(employees);
+bonus(employees);
+console.log(ratingBonus);
 
 //check for employee Number length with loop 
 // if employee number = 4 digits then recieve addtional 5%
@@ -60,18 +61,54 @@ function bonus(employeesArray){
 
 function seniority(additionalBonus) {
   for (let i = 0; i < additionalBonus.length; i++) {
-    if(additionalBonus[i].employeeNumber.length == 4) {
-      console.log('additionalBonus', additionalBonus[i].annualSalary * .05); 
+    if (additionalBonus[i].employeeNumber.length == 4) {
+      addBonus.push(additionalBonus[i].annualSalary * .05);
     }
     else {
-      console.log('no additional bonus');
+      addBonus.push('No bonus');
     }
-  
+  }
 }
-}
-
 seniority(employees);
+console.log(addBonus);
 
+function addingBonus(bonusAdd) {
+  
+  for (let i = 0; i < bonusAdd.length; i++) {
+   
+    // dot notation
+    console.log(bonusAdd[i].annualSalary);
+    // also add the bonus to employee array
+    bonusAdd[i].bonuses = ratingBonus[i];
+    console.log(bonusAdd[i]);
+  }
+}
+addingBonus(employees);
+
+function addingSeniorityBonus(bonusAddAdditional) {
+
+  for (let i = 0; i < bonusAddAdditional.length; i++) {
+
+    // dot notation
+    console.log(bonusAddAdditional[i].annualSalary);
+    // also add the bonus to employee array
+    bonusAddAdditional[i].senorityBonus = addBonus[i];
+    console.log(bonusAddAdditional[i]);
+  }
+}
+addingSeniorityBonus(employees);
+
+
+function highRoller(tooMuch){
+  for (let i = 0; i < tooMuch.length; i++){
+    if(tooMuch[i].annualSalary > 65000 && tooMuch.reviewRating >= 3){
+      console.log(tooMuch[i].annualSalary * .01);
+      
+    }
+  }
+}
+
+highRoller(employees);
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
 // What is the fewest lines of code I can write and test to get just a little closer?
@@ -79,5 +116,6 @@ seniority(employees);
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+console.log(employees);
 
 
